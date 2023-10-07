67cad519fcbb5c74f712841a3d55ce4b016d6049
Pattu02 <snthineshraj2002@gmail.com>
2023-10-07 21:01:17 +0530

    firstcommit

--- JAVASCRIPT/html1.js
+++ JAVASCRIPT/html1.js
@@ -1,4 +1,20 @@
 let btnElem = document.getElementById("btn")
+let prtElem = document.getElementById("parent")
+let intElem = document.getElementById("int")
+let no = 1
 btnElem.addEventListener("click", function () {
-    return document.body.append(document.getElementById("int").value)
+    let newElem = document.createElement("p")
+    if (intElem.value != " ") {
+        prtElem.append(newElem)
+        newElem.append(no + "." + intElem.value +" ")
+    }
+    intElem.value = " "
+    no++
+    let newbtn = document.createElement("button")
+    newbtn.append("x")
+    newElem.append(newbtn)
+    return newbtn.addEventListener("click", function () {
+        return newElem.remove()
+    })
 })
+
