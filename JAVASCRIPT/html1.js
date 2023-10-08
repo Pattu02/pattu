9531b919a707529c2b5fc54d2edb9059305881d1
Pattu02 <snthineshraj2002@gmail.com>
2023-10-08 20:53:36 +0530

    first commit

--- JAVASCRIPT/html1.js
+++ JAVASCRIPT/html1.js
@@ -3,18 +3,21 @@ let prtElem = document.getElementById("parent")
 let intElem = document.getElementById("int")
 let no = 1
 btnElem.addEventListener("click", function () {
-    let newElem = document.createElement("p")
-    if (intElem.value != " ") {
+    if (intElem.value !== "") {
+        let newElem = document.createElement("p")
         prtElem.append(newElem)
-        newElem.append(no + "." + intElem.value +" ")
+        newElem.append(no + "." + intElem.value + " ")
+        no++
+        intElem.value = ""
+        let newbtn = document.createElement("button")
+        newbtn.append("x")
+        newElem.append(newbtn)
+        return newbtn.addEventListener("click", function () {
+            return newElem.remove()
+        })
     }
-    intElem.value = " "
-    no++
-    let newbtn = document.createElement("button")
-    newbtn.append("x")
-    newElem.append(newbtn)
-    return newbtn.addEventListener("click", function () {
-        return newElem.remove()
-    })
+    else {
+        alert("***  Type Something!!!  ***")
+    }
 })
 
